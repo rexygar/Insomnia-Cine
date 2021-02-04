ecadf4efd7f1a1fa9a5d4185cbe92e15f9629c7b
javo-desu <32529489+javo-desu@users.noreply.github.com>
2021-02-04 12:54:09 -0300

    vista crear pelicula

--- resources/views/APP/Peliculas/Create.blade.php
+++ resources/views/APP/Peliculas/Create.blade.php
@@ -5,60 +5,78 @@
 
 @section('content')
 <style>
-.input-ctrl-bg{
-    background-color: aliceblue;
-}
-.input-ctrl-bg:focus {
-    background-color: aliceblue;
-}
+    .input-ctrl-bg {
+        background-color: aliceblue;
+    }
+
+    .input-ctrl-bg:focus {
+        background-color: aliceblue;
+    }
+    .sect-1{
+        margin-top: -50px !important;
+    }
 
 </style>
-<div class="container-fluid">
+<div class="container-fluid sect-1">
     <div class="row">
         <div class="col offset-1">
-            <h1>Agregar Pelicula</h1>
-        <br>
-        <br>
+            <h1 style="color: black;
+            font-weight: bold;">Agregar Pelicula</h1>
+            <br>
+            <br>
         </div>
         <div class="col-md-12">
             <form method="POST" action="" enctype="multipart/form-data">
-                <div class="col-md-10 offset-lg-2">
+                <div class="col-md-11 offset-lg-1">
                     {{ csrf_field() }}
                     <div class="form-group col-md-8">
                         <label for="Titulo">Titulo:</label>
                         <input required placeholder="ingrese el Titulo de la publicacion" type="input"
-                        class="form-control input-ctrl-bg" id="Titulo" name="Titulo">
+                            class="form-control input-ctrl-bg" id="Titulo" name="Titulo">
                     </div>
                     <div class="form-group col-md-8">
                         <label for="Sinopsis "> Sinopsis:</label>
-                        <input required placeholder="ingrese una Sinopsis" type="input" class="form-control input-ctrl-bg"
-                            id="Sinopsis" name="Sinopsis">
+                        <textarea required placeholder="ingrese una Sinopsis" class="form-control input-ctrl-bg"
+                            id="Sinopsis" name="Sinopsis" rows="10" cols="50">Ingrese una Sinopsis</textarea>
                     </div>
-                    
-                    <div class="form-group col-md-8">
-                        <label for="Genero">Genero:</label>
-                        <select class="form-control input-ctrl-bg" name="Genero" id="Genero">
-                            <option value="0">--- Seleccionar Genero ---</option>
-                            {{-- @foreach ($Genero as $cate)
-                            <option value="{{ $cate->idGenero }}"> {{ $cate ->Genero }}</option>
-                            @endforeach--}}
-                        </select>
+
+                    <div class="container">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="form-group col-md-4">
+                                    <label for="Genero">Genero:</label>
+                                    <select class="form-control input-ctrl-bg" name="Genero" id="Genero">
+                                        <option value="0">--- Seleccionar Genero ---</option>
+                                        {{-- @foreach ($Genero as $cate)
+                                    <option value="{{ $cate->idGenero }}"> {{ $cate ->Genero }}</option>
+                                        @endforeach--}}
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="Clasificacion">Clasificacion:</label>
+                                    <select class="form-control input-ctrl-bg" name="Subcat" id="Subcat">
+                                        <option value="0">--- Seleccionar Clasificacion ---</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group col-md-8">
-                        <label for="Clasificacion">Clasificacion:</label>
-                        <select class="form-control input-ctrl-bg" name="Subcat" id="Subcat">
-                            <option value="0">--- Seleccionar Clasificacion ---</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-8">
-                        <label for="Calificacion">Calificacion:</label>
-                        <select class="form-control input-ctrl-bg" name="Subcat" id="Subcat">
-                            <option value="0">--- Seleccionar Calificacion ---</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-8">
-                        <label for="Año_estreno">Año_estreno:</label>
-                        <input type="date" name="Año_estreno" id="Año_estreno">
+                    <div class="container">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="form-group col-md-4">
+                                    <label for="Calificacion">Calificacion:</label>
+                                    <select class="form-control input-ctrl-bg" name="Subcat" id="Subcat">
+                                        <option value="0">--- Seleccionar Calificacion ---</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="Año_estreno">Año_estreno:</label>
+                                    <input class="form-control input-ctrl-bg" type="date" name="Año_estreno"
+                                        id="Año_estreno">
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     {{-- <div class="form-group col-md-6">
@@ -76,22 +94,22 @@
                             </label>
                         </div> --}}
                     </div>
-                    <div class="row">
+                    <!-- <div class="row">
                         <div class="input-group control-group increment">
-                            <div class="form-group col-md-2">
-                                <label class="btn btn-default btn-file">
+                            <div class="form-group ">
+                                <label class="btn btn-default">
                                     Subir Imagen <input type="file" onchange="readURL(this);" name="image[]" multiple
                                         class="form-control input-ctrl-bg">
                                 </label>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
 
                 </div>
             </form>
         </div>
     </div>
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-md-12">
             <h3>Listing Images</h3>
 
@@ -101,7 +119,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 
 @stop
